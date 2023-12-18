ccccb7d68eef90874fc9e7339d6f57859df8cf56
Atul Singh <atulshekhawat21@gmail.com>
2023-12-18 16:55:39 +0530

    Adding Eureka Service

--- Capstone_Project/UserRegisterService/src/main/java/com/niit/UserRegisterService/UserRegisterServiceApplication.java
+++ Capstone_Project/UserRegisterService/src/main/java/com/niit/UserRegisterService/UserRegisterServiceApplication.java
@@ -2,9 +2,10 @@ package com.niit.UserRegisterService;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
-
+@EnableEurekaClient
 public class UserRegisterServiceApplication {
 
 	public static void main(String[] args) {

2297b3f1b4cfa9dbe1d42d1e2f7b6a8e9796e9cc
Atul Singh <atulshekhawat21@gmail.com>
2023-12-23 12:41:51 +0530

    Designing form for userRegistration

--- kanban_UI/src/app/app.module.ts
+++ kanban_UI/src/app/app.module.ts
@@ -5,7 +5,6 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HomeSectionComponent } from './home-section/home-section.component';
-import { KanbanSectionComponent } from './kanban-section/kanban-section.component';
 import { HeaderSectionComponent } from './header-section/header-section.component';
 import { FooterSectionComponent } from './footer-section/footer-section.component';
 import { UserLoginComponent } from './user-login/user-login.component';
@@ -16,16 +15,17 @@ import { MatButtonModule } from '@angular/material/button';
 import {MatToolbarModule} from '@angular/material/toolbar';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
+import { DashboardComponent } from './dashboard/dashboard.component';
 @NgModule({
   declarations: [
     AppComponent,
     HomeSectionComponent,
-    KanbanSectionComponent,
     HeaderSectionComponent,
     FooterSectionComponent,
     UserLoginComponent,
     UserRegisterComponent,
-    HeroSectionComponent
+    HeroSectionComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule,

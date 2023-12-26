3d9bcda6cbc066fd015d5f93e3243b084837b568
Atul Singh <atulshekhawat21@gmail.com>
2023-12-27 01:03:29 +0530

    Implementing Drap and Drop Feature to move tasks

--- kanban_UI/src/app/dashboard/dashboard.component.ts
+++ kanban_UI/src/app/dashboard/dashboard.component.ts
@@ -1,72 +1,80 @@
 import { Component } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
 import { AddTaskComponent } from '../add-task/add-task.component';
-import { CdkDragDrop, CdkDropList, moveItemInArray, transferArrayItem } from '@angular/cdk/drag-drop';
+import {
+  CdkDragDrop,
+  CdkDropList,
+  moveItemInArray,
+  transferArrayItem,
+} from '@angular/cdk/drag-drop';
 import { TaskService } from '../services/task.service';
+import { AnimationPlayer } from '@angular/animations';
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
-  styleUrls: ['./dashboard.component.css']
+  styleUrls: ['./dashboard.component.css'],
 })
 export class DashboardComponent {
   data: Array<any> = [];
+  currentTask: any;
+  constructor(public dialog: MatDialog, private userData: TaskService) {}
 
-constructor(public dialog: MatDialog,private userData:TaskService){}
-
-ngOnInit(){
-  // this.userData.viewTask().subscribe((data:any) => {
-  //   this.data=data;
-  //   console.log(data);
-  // });
-  this.refreshTask();
-}
-
-refreshTask(){
-  this.userData.viewTask().subscribe((data:any) => {
-    this.data=data;
-    console.log(data);
-  });
-}
-
-// filter the task data (By status)
-filterTasks(status: string){
-  return this.data?.filter(m=>m.status==status);
-
-}
-
-deleteTask(id:any){
-  this.userData.deleteTask(id).subscribe((resp)=>{
-    console.log(resp);
+  ngOnInit() {
+    // this.userData.viewTask().subscribe((data:any) => {
+    //   this.data=data;
+    //   console.log(data);
+    // });
     this.refreshTask();
-  },
-  (err)=>{
-    console.log(err);
   }
-  )
-}
 
-onDragStart(){
+  refreshTask() {
+    this.userData.viewTask().subscribe((data: any) => {
+      this.data = data;
+      console.log(data);
+    });
+  }
 
-}
+  // filter the task data (By status)
+  filterTasks(status: string) {
+    return this.data?.filter((m) => m.status == status);
+  }
 
+  deleteTask(id: any) {
+    this.userData.deleteTask(id).subscribe(
+      (resp) => {
+        console.log(resp);
+        this.refreshTask();
+      },
+      (err) => {
+        console.log(err);
+      }
+    );
+  }
 
+  onDragStart(task: any) {
+    this.currentTask = task;
+  }
 
-// ---------------------------------------------------------------//
-// This is for Dialog
-openDialog() {
-  const dialogRef = this.dialog.open(AddTaskComponent);
+  onDrop($event: any, status: string) {
+    const record = this.data.find((m) => m.taskId == this.currentTask.taskId);
+    if (record != undefined) {
+      record.status = status;
+    }
+    this.currentTask = null;
+  }
 
-  dialogRef.afterClosed().subscribe(result => {
-    console.log(`Dialog result: ${result}`);
-  });
-}
+  onDragOver(event: any) {
+    event.preventDefault();
+  }
 
+  // ---------------------------------------------------------------//
+  // This is for Dialog
+  openDialog() {
+    const dialogRef = this.dialog.open(AddTaskComponent);
 
-
-
-
-
-
-
+    dialogRef.afterClosed().subscribe((result) => {
+      console.log(`Dialog result: ${result}`);
+    });
+  }
 }
